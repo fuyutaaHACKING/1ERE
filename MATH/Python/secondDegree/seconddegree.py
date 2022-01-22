f9cb1e4a8579a103faebfd8834928d18e70788fe
n0l1um <d1rtysh3ll.0002@gmail.com>
2022-01-22 12:54:03 +0100

    1.2

---
+++ MATH/Python/secondDegree/seconddegree.py
@@ -0,0 +1,13 @@
+'''
+forme canonique: 
+f(x)= a(x - alpha)²+B
+
+alpha = -b/2a
+
+Beta = -(b²-4ac)/4a
+
+P = x1 * x2 = c/a
+S = x1 + x2 = -b/a
+
+delta = 2²-4ac
+'''
---
+++ MATH/Python/termeSuiteEnzo.py
@@ -0,0 +1,6 @@
+un = 5
+rank = int(input("enter a rank"))
+for index in range(0, rank):
+    un = un * 2
+    un = un + 3
+    print(un)
