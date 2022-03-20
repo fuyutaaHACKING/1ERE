810b143f984e673648b4cb7a810d78f191a4a4be
n0l1um <98127481+n0l1um@users.noreply.github.com>
2022-03-20 10:17:43 +0100

    Delete NSI/Python directory

--- NSI/Python/DMs/DM01/superiorToZeroOrElse.py
+++
@@ -1,8 +0,0 @@
-inputData = input("Entrez un nombre :")
-
-inputInt = int(inputData)
-
-if inputInt > 0:
-    print("Le chiffre rentré est supérieur à 0")
-else:
-    print("Le chiffre rentré est inférieur ou égal à 0")
