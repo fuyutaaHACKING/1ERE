4d04a51b1749b0a8556109cab313a2be9a69a44d
n0l1um <d1rtysh3ll.0002@gmail.com>
2022-03-24 21:30:32 +0100

    Update rechercheMinimum.py

--- NSI/PYTHON/listes/rechercheMinimum.py
+++ NSI/PYTHON/listes/rechercheMinimum.py
@@ -1,3 +1,5 @@
+# tri par selection, fonction basique
+'''
 def recherche_minimum(liste, debut):
     for i in range(len(liste)):
         for j in range(debut, len(liste)):
@@ -7,3 +9,16 @@ def recherche_minimum(liste, debut):
 
 listOfValues = [8,1,5,6,2]
 print(recherche_minimum(listOfValues, 1))
+'''
+
+# Devoir demandé: fonction qui renvoie la valeur minimale de la liste
+
+def recherche_minimum(liste, debut):
+    for i in range(len(liste)):
+        for j in range(debut, len(liste)):
+            if liste[j-1] > liste[j]:
+                liste[j-1], liste[j] = liste[j], liste[j-1]
+    return liste[0]
+
+listOfValues = [8,1,5,6,2]
+print("La valeur la plus petite de la liste est:",recherche_minimum(listOfValues, 1))
