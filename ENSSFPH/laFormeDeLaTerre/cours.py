b6f0a0d23344704441ade4003d748a27fb9d89d6
n0l1um <d1rtysh3ll.0002@gmail.com>
2022-04-07 22:49:35 +0200

    up

---
+++ ENSSFPH/laFormeDeLaTerre/cours.py
@@ -0,0 +1,33 @@
+'''
+I) La terre est elle ronde?
+Trouvez des exemples de pourquoi la terre est plate.
+- Les ellipses
+- Les bateaux et leur coque qui disparait avant le mat une fois distantes
+- Les constellations ne sont pas les mêmes pole nord et sud
+- L'ombre de la terre sur la Lune
+- L'ombre d'un baton à la même heure à la même lattitude
+
+GMT = Greenwich Mean Time
+
+Bilan: 
+La terre peut être aussimilée à une sphère, mais elle est cependant aplatie aux pôles.
+
+II) Longueur d'un méridien: 
+
+1) Méridiens parallèles
+Pour se repérer sur terre, les hommes ont besoin de lignes imaginaires:
+- Les méridiens: coupent la Terre du nord au sud (pôles)
+- Les parallèles: prallèles à l'équateur
+---) Méridiens et parallèles sont perpendiculaires
+
+FEUILLE EXO DOUBLE COTE:
+1. Se déplacer sur Terre
+1) Repérage
+Le repérage d'un point sur la terre se fait l'aide d'un système de deux coordonnées: une latitude et une longitude (ex: coordonnées GPS)
+- La longitude est l'angle mesuré à partir du méridien de Greenwich
+- La latitude est l'angle mesuré à partir de l'équateur
+
+2) Longueurs d'arc
+a) Arc de méridien
+Lorsque deux points sont sur un même méridien, la longueur du chemin qui relie ces deux points en suivant ce méridien est proportionnelle à l'angle séparant ces deux points.
+'''
---
+++ ENSSFPH/laFormeDeLaTerre/exoRevision.py
@@ -0,0 +1,53 @@
+'''
+EXO REVISION CHAP 4
+
+--Exercice 1--
+
+On considère deux points à la surface de la terre:
+A : 20 N; 100 O
+B: 66 N; 100 O
+
+1) les points A et B sont-ils situés sur le même parallèle ou le même méridien? Justifier.
+même méridien (même longitude)
+
+2) Soit O le centre de la terre, calculer l'angle AOB.
+AOB = 66-20 = 46
+
+3) Calculer la longueur de l'arc de méridien qui relie les points A et B
+dAB = (46*4000)/360 = 5111Km
+
+
+--Exercice 2--
+
+C: Caen
+P: Pau
+Tous les deux 0.37 de longitude
+Dans chaque ville on plante un baton de 1M
+C: 38cm
+P: 27cm
+
+Longueur de l'arc de méridien: 654km
+
+1) Determiner les angles a et b.
+Pour a:
+A = 1M
+O = 27CM
+tan(a) = 0.27/1 = 0.27m
+atan(0.27) = 15.1 degrees
+
+Pour b:
+A = 1M
+O = 38cm
+
+tan(b) = 0.38/1 = 0.38
+atan(0.38) = 20.8
+
+2) Determiner la val de l'angle y sachant que y = b - a
+y = 20.8 - 15.1 = 5.7
+
+3) Determiner la circonference de la terre en utilisant la methode d'eratosthene
+5.7 -) 654km
+360 -) x
+
+x = (360*654)/5.7 = 40 000
+'''
