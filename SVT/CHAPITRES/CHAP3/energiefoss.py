3e25e6ff373ab25defd745f30fc7f24f148adfe5
n0l1um <d1rtysh3ll.0002@gmail.com>
2022-04-30 12:08:46 +0200

    up

---
+++ SVT/CHAPITRES/CHAP3/energiefoss.py
@@ -0,0 +1,37 @@
+'''
+ENERGIES FOSSILES
+Q: que devient la matière organique morte? Comment l'énergie solaire est-elle stockée à long terme ?
+
+3) la photosynthèse à l'origine des combustibles fossiles.
+
+À l'échelle des temps géologiques, une partie de la matière organique s'accumule dans les sédiments puis se transforme en donnant des combustibles fossiles (gaz, charbon, pétrole) différents selon la nature de la matière organique accumulée et les conditions de formation. Les combustibles fossiles sont des ressources énergétiques non renouvelables à l'échelle humaine.
+
+Lors de la formation de combustibles fossiles, la matière organique morte est ensevelie par les sédiments. En domaine continentale, à forte productivité primaire et marécageuse, environ 1 % des végétaux accumulés écharpe à la dégradation par des bactéries aérobies (ayant besoin de dioxygène) et vont subir des modifications très lentes conduisant à la formation de charbon. Il en est de même en domaine océanique ou le phytoplancton accumulé évoluera en pétrole ou gaz. 
+
+Le processus subi est le même : accumulation, sédimentation rapide, augmentation de la température et de la pression, action des bactéries anaérobies (n'ayant pas besoin de dioxygène pour survivre) afin de transformer la biomasse en combustible fossile.
+
+Le charbon ainsi formé constitue des veines en profondeur. Le pétrole et le gaz quant à eux stockés dans des roches réservoirs (poreuses) recouvertes de roche dit de couverture (imperméables) les emprisonnant.
+
+++—----------------------++
+++—----------------------++
+Exercice énergie fossile
+Correction exercice 1 
+La formation du pétrole nécessite l'accumulation de matière organique en profondeur subissant un phénomène de pyrolyse afin de se transformer en kérogène (riche en carbone et en hydrogène) puis en pétrole. La formation de pétrole nécessite la présence de roche réservoir perméable est poreuse dans lesquels le pétrole peut s'accumuler, l'ensemble recouvert par des roches imperméables de type marnes ou évaporites. Nous pouvons observer dans la région étudiée la présence degré en profondeur, roche poreuse, dans laquelle le pétrole pourrait se cumuler et ainsi que des roches imperméables (argiles et marnes) entourant cette strate sédimentaire.
+La région est donc exploitable, le forage devra attendre la couche de graisse pour en extraire le pétrole.
+
+Pyrolyse = décomposition chimique d'un composé organique par une augmentation importante de sa température pour obtenir d'autres produits qu'il ne contenait pas. L'opération est réalisée en l'absence d'oxygène ou en atmosphère pauvre en oxygène pour éviter l'oxydation et la combustion.
+
+Exercice 2:
+L'exploitation du gaz de schiste impact davantage l'environnement que l'exploitation de gaz naturel. En effet, le gaz naturel est stocké en profondeur (environ 1000 m) dans des poches. L'extraction nécessite donc simplement de mettre en place des puits afin d'extraire le gaz. En revanche, le gaz de schiste est stocké au sein même de roches qualifiées de roches mère. Celles-ci sont plus profondes (entre 2000 et 3000 m de profondeur). Donc depuis plus loin réalisé et induisant ensuite la fracturation de la roche. L'impact environnemental est donc élevé. Il faut envoyer de l'eau sous haute pression associée à du sable et des détergents afin de fracturer la broche et permettre la libération des gaz. Le sous-sol est ainsi fragilisé et cette démarche utilise énormément d'eau, ressource fragile. De plus, l'association de détergent induit la pollution des eaux qu'il faut ensuite traiter car contenant des sels, des métaux lourds et des éléments radioactifs. Il y a donc un risque majeur de pollution des sols et des nappes phréatiques.
+
+Exercice 3:
+Calculer la consommation annuelle moyenne de chaque énergie fossile entre 1960 et 1998 soit 38 ans :
+Pétrole: 123.10p9 / 38 = 3,25.10p9 tep/an
+Charbon: 143.10p9 / 38 = 3,76.10p9 tep/an
+Gaz naturel: 57.10p9 / 38 = 1,5.10p9 tep/an
+
+La consommation annuelle mondiale en pétrole en 2017 est de 4, 62.10p9, celle du charbon de 3,73.10p9 tep et celle du gaz naturel est de 3,16.10p9. Nous pouvons constater que la consommation annuelle de ces 3 combustibles fossiles a nettement augmenté en 2017.
+Nous pouvons formuler l'hypothèse que les besoins de la population mondiale augmentent du fait d'une industrialisation des pays en voie de développement et d'une augmentation de la population mondiale.
+Calcul du nombre d'années restant de consommation :
+Réserves prouvées / consommation annuelle
+'''
---
+++ SVT/CHAPITRES/CHAP3/photosynthese.py
@@ -0,0 +1,120 @@
+'''
+PHOTOSYNTHESE
+À l'échelle de la feuille (pour les plantes), la photosynthèse utilise une très faible fraction de la puissance radiative reçue, le reste est soit réfléchi, soit transmis, soit absorbé et dissipée (échauffement et évapotranspiration).
+
+Énergie solaire incidente 100% :
+20% : Énergie réfléchie
+69% : énergie dissipée
+10% : énergie transmise à travers la feuille
+Reste (1%) : énergie utilisée pour la photosynthèse
+
+Q: comment les produits issus de la photosynthèse sont-ils utilisés par les êtres vivants ?
+
+3) Le devenir des produits de photosynthèse
+
+La matière organique fabriquée lors de la photosynthèse des végétaux chlorophylliens, organismes autotrophes, circule dans la biosphère, elle alimente directement ou indirectement les autres êtres vivants hétérotrophes.
+
+—----------------
+Chez les êtres vivants, des molécules comme l'amidon (sucre de réserve de végétaux) sont transformées en molécules plus petites comme le glucose. Dans les cellules des animaux, des végétaux et des champignons notamment, le glucose est ensuite dégradé, en présence de dioxygène lors de la respiration cellulaire.
+
+Glucose + O2 –) H2O + CO2 + énergie directement utilisable par la cellule (fabrication sa propre MO)
+
+L'énergie libérée permet le fonctionnement, la croissance et l'entretien de l'organisme. La matière organique non transformée et perdue dans les déchets (excréments).
+
+—-------
+Réseau trophique : transfert d'énergie au sein d'une chaîne alimentaire.
+
+Énergie lumineuse:
+Végétaux (producteur primaire)
+—-) photosynthèse
+—----) consommation matière minérale + production de matière organique (exemple : amidon)
+—) 
+Herbivore (producteur secondaire)
+—-) respiration
+—----) consommation matière organique (en glucose) + production d'énergie utilisable par la cellule (perte) et de déchets (MO non transformée)
+—--)
+Carnivore (producteur secondaire)
+—) respiration 
+—--) consommation matière organique (ou glucose) + production d'énergie utilisable par la cellule + énergie thermique
+
+La biomasse est à la base des réseaux alimentaires de la plupart des écosystèmes terrestres. Le glucose ainsi fabriqué par les végétaux, suite à la photosynthèse, peut-être transformé par les êtres vivants (on parle alors de transfert de matière) réalisant ainsi leur métabolisme (respiration ou fermentation) et leur permettant de fabriquer l'énergie et les molécules nécessaires à leur fonctionnement (mouvement, croissance…)
+
+Équation bilan de la respiration : 
+C6H12O6 + 6O2 –) 6CO2 + 6H2O + E utilisable + E thermique
+
+Équation bilan de la fermentation : 
+C6H12O6 –) CO2 + composé organique + E utilisable + E thermique
+
+Il existe deux types de fermentation :
+La fermentation lactique, le composé organique fabriqué sera alors de l'acide lactique. Nos muscles peuvent réaliser la fermentation lactique. 
+La fermentation alcoolique, le composé organique fabriqué sera alors de l'éthanol. Certaines bactéries, certains champignons tels que les levures peuvent réaliser la fermentation alcoolique. 
+
+NB: la respiration produit jusqu'à 1080 kilojoules d'énergie utilisable par mode de glucose alors que la fermentation produit seulement 60 kilojoules. La fermentation est donc un processus moins efficace.
+
+++—----------------------++
+++—----------------------++
+EXERCICE : la conversion de l'énergie par la photosynthèse
+
+Les organismes chlorophylliens qui sont producteurs primaires utilisent l'énergie solaire pour fabriquer dans biomasse en réalisant la photosynthèse. Aventure la lumière, du dioxyde de carbone, d'eau et d'ions minéraux, la cellule végétale chlorophyllienne  est capable de fabriquer l'ensemble des molécules organiques (glucides, lipides, acides, nucléiques et protéines) dont elle a besoin pour vivre. 
+
+6CO2+6H2O avec lumière –) C6H12O6 + 6CO2
+
+Q: QUELLE EST LA PART DE L'ÉNERGIE SOLAIRE UTILISÉ PAR LES ORGANISMES CHLOROPHYLLIENS?
+
+Calculer la proportion de l'énergie solaire quelqu'un par les organismes photosynthétiques:
+
+Chaque année les organismes photosynthétiques produit sur l'ensemble de la terre environ 134,18 milliards de tonnes de matière organique. On appelle productivité primaire brute (PPB) la production de biomasse végétale par unité de surface et par unité de temps. 
+La surface de la terre est estimé à 510 065 700km²
+L'énergie solaire absorbée par la Terre (en éliminant l'énergie réfléchie) est en moyenne de 168 W/m², soit 168J/s/m²
+
+DOCUMENT 1: Composition de la matière et énergie contenue par type de molécule.
+
+Matière organique/ énergie contenue dans 1g
+
+Lipide / 37kJ
+Protide / 17kJ
+Glucide / 16 kJ
+Calculez la quantité d’énergie contenue dans les 134.18 milliards de tonnes de matière organique produite par seconde.
+
+Calculer la quantité d’énergie moyenne reçue par la terre en une seconde.
+Calculer le pourcentage de l’énergie solaire reçue qui est convertie par la photosynthèse en énergie chimique organique
+
+Calcul la quantité d'énergie contenue dans 1 g de matière organique:
+E = 37+17+16 = 70KJ/g
+
+Calcul de la quantité d'énergie contenue dans les 134,18 milliards de tonnes de matière organique produite par an :
+70kJ -) 1g
+? -) 134.18*10p15g car 1T = 1000kg = 1*10p6g
+
+E = 134.18*10p15 *70 = 9.38*10p18 KJ/an
+
+Calcul de la quantité d'énergie contenue dans les 134,18 milliards de tonnes de matière organique produite par seconde :
+1an = 365*24*60*60 = 31 546*10p3 s
+9.39*10p18 KJ -) 31 546*10p3 s
+? -) 1s
+
+E = 9.39*10p18 / 31.536*10p6 = 0.3*10p12 KJ/s
+
+Calcul de la quantité d'énergie moyenne reçue par la terre en une seconde.
+E = 168* 5 100 657*10p8 = 856 910 376*10p8 J/s
+
+Calcul du pourcentage de l'énergie solaire reçue qui est convertie par la photosynthèse en énergie chimique organique :
+Quantité d’énergie reçue par la terre en 1s = 8.57*10p16 J
+Quantité d’énergie contenue dans les 134.18 milliards de T de MO/s = 0.3*10p12 KJ = 0.3*1p15 J
+
+Donc 0.3*1015 -) 8.57*10p16 J
+? -) 100
+
+0.3*10p15 * 100 /8.57*10p16 = 3*10p16/8.57*10p16 = 0.35%
+
+++—----------------------++
+++—----------------------++
+La photosynthèse permet l'entrée dans la biosphère de matière minérale stockant l'énergie sous forme chimique.
+
+Équation bilan : 
+6CO2 + 6H2O = C6H12O6 + 6O2
+
+matière minérale –) matière organique (glucose) + matière minérale
+
+La courbe du spectre d'absorption de la chlorophylle et la courbe d'action sont très proches. La chlorophylle absorbe donc les rayons pour faire la photosynthèse.
+'''
